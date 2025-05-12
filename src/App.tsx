aa49bdc8cbb5ce6ae444c876abe9d321aa78f19e
Dipak-Gautam <anjangautam095@gmail.com>
2025-05-12 15:51:24 +0545

    feat: navigation setup completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomeScreen from "./Components/HomeScreen/HomeScreen";
+import Cart from "./Components/Cart/Cart";
+
+const router = createBrowserRouter([
+  { path: "/", element: <HomeScreen /> },
+  { path: "/cart", element: <Cart /> },
+]);
+
 function App() {
   return (
-    <>
-      <div>Hello from home</div>
-    </>
+    <div>
+      <RouterProvider router={router} />
+    </div>
   );
 }
 
